99a34dd0ea230c865fe00043c57ebd160e2a2185
Victor <vicbermad@alu.edu.gva.es>
2022-11-16 18:32:58 +0100

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Estudiante " + args[0] + ":");
+
+        Veredicto.veredicto (args[1], args[2]);
+    }
+}
---
+++ src/Veredicto.java
@@ -0,0 +1,13 @@
+public class Veredicto {
+    public static void veredicto(String valor1, String valor2) {
+        if (Integer.valueOf(valor1) > Integer.valueOf(valor2)) {
+            System.out.println("Te faltan horas de estudio para cada asignatura, espabila.");
+        }
+        else if (valor1.equals(valor2)) {
+            System.out.println("Estudias una hora de media por cada asignatura, puede que sea insuficiente, sigue espabilando.");
+        }
+        else {
+            System.out.println("Llevas las asignaturas al día, has espabilado.");
+        }
+    }
+}
